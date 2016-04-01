ceeca97d586ae25ed766c9f5b55cb8dddaa34b71
Sean Doyle <sean.p.doyle24@gmail.com>
2016-04-01 14:06:09 -0700

    Setup Heroku
    This commit resolves missing environment variables from Heroku in
    preparation for deploying to `staging` and `production`.
    
    In addition to these code changes, the following commands were executed:
    
    ```bash
    $  staging config:set SMTP_ADDRESS=garbage SMTP_DOMAIN=garbage SMTP_PASSWORD=garbage SMTP_USERNAME=garbage APPLICATION_HOST=freshfoodconnect-staging.herokuapp.com
    $  production config:set SMTP_ADDRESS=garbage SMTP_DOMAIN=garbage SMTP_PASSWORD=garbage SMTP_USERNAME=garbage APPLICATION_HOST=freshfoodconnect-production.herokuapp.com
    ```
    
    In the future (once we implement Email functionality), the
    `SMTP_{ADDRESS,DOMAIN,PASSWORD,USERNAME}` values will need to be
    corrected, and the `APPLICATION_HOST` should be configured to point to
    the Fresh Food Connect domain.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,7 +1,7 @@
 require_relative "production"
 
 Mail.register_interceptor(
-  RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"))
+  RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS", "")),
 )
 
 Rails.application.configure do
