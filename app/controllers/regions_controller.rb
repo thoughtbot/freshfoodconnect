de8a66fe6546b84b2f082ed6076cbe80114c1d1c
Murtaza Gulamali <mygulamali@gmail.com>
2016-08-19 17:23:27 +0100

    Admins can create new regions

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,9 +1,32 @@
 class RegionsController < ApplicationController
+  def create
+    @region = Region.new(region_params)
+
+    if @region.save
+      redirect_to(
+        regions_path,
+        flash: { success: t(".success", name: @region.name) }
+      )
+    else
+      render :new
+    end
+  end
+
   def index
     @regions = Region.order(:name).all
   end
 
+  def new
+    @region = Region.new
+  end
+
   def show
     @region = Region.find(params[:id])
+  end
+
+  private
+
+  def region_params
+    params.require(:region).permit(:name)
   end
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,4 @@
 class Region < ActiveRecord::Base
   has_many :zones
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
